1cbe722411c5f8e520736a79b8a2b91f6a0f32c7
issuser <issuser@issuserdemacbook-pro-2.local>
2017-02-15 21:52:06 +0800

    修改分支文件

--- AFNHttpHelper/Basic/AppDelegate.m
+++ AFNHttpHelper/Basic/AppDelegate.m
@@ -17,7 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
+// 分支提交
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     UINavigationController * nav = [[UINavigationController alloc] initWithRootViewController:[[MainViewController alloc] init]];
